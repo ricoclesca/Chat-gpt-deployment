444b42f6a5918d65bb092204fe7fdb21012545ee
ricoclesca <ricardo.clesca@icloud.com>
2025-04-21 20:55:45 -0400

    main

--- Eks-terraform/backend.tf
+++ Eks-terraform/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket = "demo1212221" # Replace with your actual S3 bucket name
+    bucket = "gpt-deployment-rico" # Replace with your actual S3 bucket name
     key    = "EKS/terraform.tfstate"
-    region = "ap-south-1"
+    region = "us-east-1"
   }
 }
